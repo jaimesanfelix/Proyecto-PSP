38c0db5d2f71cf0ece8a2c8a36cc951e20f64f99
jaimesanfelix <jaimesanfelix7@gmail.com>
2023-05-02 10:37:14 +0200

    Update Fase3

--- ClienteSocket.java
+++ ClienteSocket.java
@@ -1,26 +1,32 @@
 import java.io.*;
 import java.net.*;
+import java.security.Key;
 import java.util.Scanner;
+
+import utils.KeysManager;
+import utils.RSASender;
 
 public class ClienteSocket {
 
     private static final String DNSAWS = "ec2-3-237-199-99.compute-1.amazonaws.com";
     //private static final String DNSAWS = "localhost";
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws Exception {
         Socket socket;
         ObjectOutputStream salida;
         String frase;
+        Key clavePublica;
         
         socket = new Socket(DNSAWS, 11000);
         salida = new ObjectOutputStream(socket.getOutputStream());
+        clavePublica = KeysManager.getClavePublica();
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduce tu usuario: ");
         String usuario = sc.nextLine();
         String u1 = usuario.substring(0, 1).toUpperCase();
         String nombreUsuario = u1 + usuario.substring(1);
-        salida.writeObject(nombreUsuario);
+        salida.writeObject(RSASender.cipher(nombreUsuario, clavePublica));
         
         WorkerCliente wc = new WorkerCliente(socket, nombreUsuario);
         wc.start();
@@ -30,7 +36,7 @@ public class ClienteSocket {
             
             frase = sc.nextLine();
             //System.out.println(nombreUsuario + ">" + frase);
-            salida.writeObject(frase);
+            salida.writeObject(RSASender.cipher(frase, clavePublica));
         } while (!frase.contains("exit"));
 
     }
