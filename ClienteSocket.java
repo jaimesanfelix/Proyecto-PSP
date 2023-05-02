0f67c9fbccf85b7760be9e698c0115dff1ccdf44
jaimesanfelix <jaimesanfelix7@gmail.com>
2023-05-02 10:55:51 +0200

    Update ruta aws

--- ClienteSocket.java
+++ ClienteSocket.java
@@ -8,7 +8,7 @@ import utils.RSASender;
 
 public class ClienteSocket {
 
-    private static final String DNSAWS = "ec2-3-237-199-99.compute-1.amazonaws.com";
+    private static final String DNSAWS = "ec2-3-237-240-193.compute-1.amazonaws.com";
     //private static final String DNSAWS = "localhost";
 
     public static void main(String[] args) throws Exception {
